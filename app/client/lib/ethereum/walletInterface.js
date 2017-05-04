4cf54ead5512c0fadd2f4d70312cc11862eff269
Marty McFly <martymcfly2016@protonmail.com>
2017-05-04 17:27:16 +0200

    geändert:       app/.meteor/versions 	geändert:       app/client/index.html 	geändert:       app/client/lib/appStart.js 	geändert:       app/client/lib/collections.js 	geändert:       app/client/lib/ethereum/walletInterface.js

--- app/client/lib/ethereum/walletInterface.js
+++ app/client/lib/ethereum/walletInterface.js
@@ -39,7 +39,7 @@ web3.eth.getBlock(0, function(e, res){
             case '0x0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303':
                 Session.set('network', 'test');
                 break;
-            case '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3':
+            case '0x40cb5b4035ddab4bc17b5d75bb0d8dcca962263fe7e336173734a6bd3163a001':
                 Session.set('network', 'main');
                 break;
             default:
